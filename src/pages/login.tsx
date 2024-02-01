8329d40800910f60132bafccc0ea56afee3045d3
dsaauser4 <dsaauser4@gmail.com>
2024-02-01 12:27:36 +0700

    :fire: Remove Unused Icon

---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import LoginPage from "@/templates/Auth/LoginPage";
+import type { NextPage } from "next";
+
+const Login: NextPage = () => {
+  return <LoginPage />;
+};
+
+export default Login;
---
+++ src/templates/Auth/LoginPage/index.tsx
@@ -0,0 +1,24 @@
+import { LoginForm } from "@/stories/organism/Forms/LoginForm/LoginForm";
+import React, { useState } from "react";
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add your login logic here
+  };
+
+  return <LoginForm></LoginForm>;
+};
+
+export default LoginPage;
