264ad36218c6c5f10d5fd9d548c14bba12ce2c7d
dsaauser4 <dsaauser4@gmail.com>
2024-02-05 13:26:38 +0700

    :lipstick: Create Aside Menu

---
+++ src/stories/molecules/Aside/AsideMenu/AsideMenu/AsideMenu.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import clsx from "clsx";
+import { AsideMenu } from "./AsideMenu";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Molecules/AsideMenu/AsideMenu",
+  component: AsideMenu,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+
+} satisfies Meta<typeof AsideMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ActiveItem: Story = {
+  args: {
+  },
+};
