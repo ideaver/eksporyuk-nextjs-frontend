ba349f0c6359eb07d2089a4ce2e49efc8a8b6ffa
dsaauser4 <dsaauser4@gmail.com>
2024-02-05 13:30:35 +0700

    :lipstick: Create Aside Default

---
+++ src/stories/molecules/Aside/AsideDefault/AsideDefault.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import clsx from "clsx";
+import { AsideDefault } from "./AsideDefault";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Molecules/AsideMenu/AsideDefault",
+  component: AsideDefault,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+} satisfies Meta<typeof AsideDefault>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ActiveItem: Story = {
+  args: {},
+};
