5578dad23326557c786e49cc63dc35cc426be651
dsaauser4 <dsaauser4@gmail.com>
2024-02-05 13:03:09 +0700

    :lipstick: Create Aside Toolbar

---
+++ src/stories/molecules/Aside/AsideMenuItems/AsideToolbar/AsideToolbar.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { AsideToolbar } from "./AsideToolbar";
+import clsx from "clsx";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Molecules/AsideMenu/AsideToolbar",
+  component: AsideToolbar,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  render: (args) => (
+    // <div className="aside">
+    <div
+      className="aside-toolbar flex-column-auto bg-dark"
+      id="kt_aside_toolbar"
+    >
+      <AsideToolbar></AsideToolbar>
+    </div>
+    // </div>
+  ),
+} satisfies Meta<typeof AsideToolbar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    buttonColor: "primary",
+  },
+};
+export const Display: Story = {
+  render: (args) => (
+    <div className="aside">
+      <div className="aside-toolbar flex-column-auto" id="kt_aside_toolbar">
+        <AsideToolbar></AsideToolbar>
+      </div>
+    </div>
+  ),
+  args: {
+    buttonColor: "primary",
+  },
+};
