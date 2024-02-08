805dd6f1c99c8e5cbf593b829ef0a3825be41d05
dsaauser12 <dsaauser12@gmail.com>
2024-02-08 13:11:12 +0700

    :lipstick: Update Order Details

--- src/stories/organism/Tables/BuyerDetails/BuyerDetails.stories.tsx
+++ src/stories/organism/Tables/BuyerDetails/BuyerDetails.stories.tsx
@@ -1,11 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { OrderDetails } from "./OrderDetails";
+import { BuyerDetails } from "./BuyerDetails";
 import { PaymentConfirmation, PaymentMethod } from "@/types/general/payment-method";
-import { Badge } from "@/stories/atoms/Badge/Badge";
 
 const meta = {
-  title: "Organism/OrderDetails",
-  component: OrderDetails,
+  title: "Organism/BuyerDetails",
+  component: BuyerDetails,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
@@ -13,21 +12,31 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-} satisfies Meta<typeof OrderDetails>;
+} satisfies Meta<typeof BuyerDetails>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Display: Story = {
-  render: (args) => <OrderDetails {...args} />,
+  render: (args) => <BuyerDetails {...args} />,
   args: {
+    items: [
+      {
+        icon: "/media/svg/card-logos/profil.svg",
+        title: "Nama",
+        value: 'Abdul Halim Abdullah',
+      },
+      {
+        icon: "/media/svg/card-logos/email.svg",
+        title: "Email",
+        value: 'abdulhalim@gmail.com',
+      },
+      {
+        icon: "/media/svg/card-logos/telepon.svg",
+        title: "No. Telepon",
+        value: '+6141 234 567',
+      },
+    ],
     className: "w-100",
-    invoiceNumber: "7196",
-    subscription: 'Berlangganan - Awal',
-    transactionDate: '13/01/2023',
-    buttonValue: 'Aa',
-    statusPayment: PaymentConfirmation.Confirmed,
-    buttonPayment: PaymentMethod.Mandiri,
-    
   },
 };
--- src/stories/organism/Tables/OrderDetails/OrderDetails.tsx
+++ src/stories/organism/Tables/OrderDetails/OrderDetails.tsx
@@ -4,57 +4,36 @@ import { useThemeMode } from "@/_metronic/partials";
 import { Badge } from "@/stories/atoms/Badge/Badge";
 import { Buttons } from "@/stories/molecules/Buttons/Buttons";
 import { PaymentConfirmation, PaymentMethod } from "@/types/general/payment-method";
+import { subscribe } from "diagnostics_channel";
 import { useEffect, useRef, useState } from "react";
 
 
 
 type OrderDetailsProps = {
     className: string;
-    title?: string;
-    items: ListOrderDetails[];
-    statusPayment?: JSX.Element
-    buttonPayment?: JSX.Element
-    paymentMethod?: PaymentMethod
+    statusPayment?: PaymentConfirmation
+    buttonPayment?: PaymentMethod
+    invoiceNumber?: string
+    transactionDate?: string
+    subscription?: string
+    buttonValue?: string
 };
 
 const OrderDetails: React.FC<OrderDetailsProps> = ({
-    className,
-    title = "Order Details (INV 7196)",
-    items,
-    statusPayment = (
-        <Badge
-            badgeColor="success"
-            label="Paid"
-        />
-    ),
-    buttonPayment = (
-        <Buttons
-            buttonColor="secondary"
-        />
-    ),
-    paymentMethod,
+    className = "w-100",
+    statusPayment = PaymentConfirmation.Confirmed,
+    buttonPayment = PaymentMethod.Mandiri,
+    invoiceNumber = "7196",
+    transactionDate = '13/01/2023',
+    subscription = 'Berlangganan - Awal',
+    buttonValue = 'Aa'
 }) => {
     const chartRef = useRef<HTMLDivElement | null>(null);
     const { mode } = useThemeMode();
-    const [shouldRender, setShouldRender] = useState(false);
-    function isIncludedPaymentMethod(value: string): value is PaymentMethod {
-        return Object.values(PaymentMethod).includes(value as PaymentMethod);
-    }
-    function isPaymentConfirmed(value: string): value is PaymentConfirmation {
-        return Object.values(PaymentConfirmation).includes(value as PaymentConfirmation);
-    }
 
-    useEffect(() => {
-        // Set shouldRender to true when any of the props change
-        setShouldRender(true);
-    }, [
-        title,
-    ]);
 
-    // Reset shouldRender to false after rendering
-    useEffect(() => {
-        setShouldRender(false);
-    }, [shouldRender]);
+
+
 
 
     return (
@@ -64,7 +43,7 @@ const OrderDetails: React.FC<OrderDetailsProps> = ({
                 {/* begin::Stats */}
                 <div className="card-header border-0 pt-5">
                     <h3 className="card-title align-items-start flex-column">
-                        <span className="card-label fw-bold fs-3 mb-1">{title}</span>
+                        <span className="card-label fw-bold fs-3 mb-1">Order Details (INV {invoiceNumber})</span>
                     </h3>
                 </div>
                 {/* end::Header */}
@@ -84,71 +63,120 @@ const OrderDetails: React.FC<OrderDetailsProps> = ({
                             {/* end::Table head */}
                             {/* begin::Table body */}
                             <tbody>
-                                {items.map((item, index) => (
-                                    <tr
-                                        key={index}
-                                    >
-                                        <th className="">
-                                            <div className="d-flex align-items-center mt-2">
-                                                <div className="symbol symbol-30px me-2">
-                                                    <img
-                                                        src={item.icon}
-                                                        className=""
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <div className="d-flex flex-column">
-                                                    <p className="text-muted fw-bold text-hover-primary text-start fs-6 mb-0">
-                                                        {item.title}
-                                                    </p>
-                                                </div>
+                                <tr>
+                                    <th className="">
+                                        <div className="d-flex align-items-center mt-2">
+                                            <div className="symbol symbol-30px me-2">
+                                                <img
+                                                    src={'/media/svg/card-logos/date-orderdetails.svg'}
+                                                    className=""
+                                                    alt=""
+                                                />
                                             </div>
-                                        </th>
-                                        {/* <td className="">
-                                            <div className="d-flex items-center">
-                                                <div className="symbol symbol-30px">
-                                                    <img
-                                                        src={item.icon}
-                                                        className="text-end"
-                                                        alt=""
-                                                    />
-                                                </div>
-                                                <p
-                                                    className="text-muted fw-bold text-hover-primary min-w-50 text-start pt-2 ps-2 fs-6"
-                                                >
-                                                    {item.title}
+                                            <div className="d-flex flex-column">
+                                                <p className="text-muted fw-bold text-hover-primary text-start fs-6 mb-0">
+                                                    Tanggal Pembelian
                                                 </p>
                                             </div>
-
-                                        </td> */}
-                                        <td>
-                                            <div className="d-flex flex-column w-100 me-2">
-                                                <div
-                                                    className={``}
-                                                >
-                                                    <div
-                                                        className={`text-end fw-bold fs-5 ${isIncludedPaymentMethod(item.value) ? "text-dark" : "text-muted"}`}
-                                                    >
-                                                        {isIncludedPaymentMethod(item.value) && <Buttons buttonColor="secondary" classNames="me-4 btn-sm fw-bold fs-4">Aa</Buttons>}
-                                                        {item.value === "Pembayaran Dikonfirmasi" &&
-                                                            <Badge
-                                                                badgeColor={(isPaymentConfirmed(item.value)) ? "success" : "danger"}
-                                                                label={item.value}
-                                                            />}
-                                                        {item.value !== "Pembayaran Dikonfirmasi" && (
-                                                            item.value
-                                                        )}
-                                                    </div>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div className="d-flex flex-column w-100 me-2">
+                                            <div className={``}>
+                                                <div className={`text-end fw-bold fs-5 text-muted `}>
+                                                    {transactionDate}
                                                 </div>
                                             </div>
-                                        </td>
-                                        {/* <td className="">
-                      <span className="text-muted text-start fs-7 fw-semibold">
-                        {item.precentageValue}%
-                      </span>
-                    </td> */}
-                                    </tr>
-                                ))}
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="">
+                                        <div className="d-flex align-items-center mt-2">
+                                            <div className="symbol symbol-30px me-2">
+                                                <img
+                                                    src={'/media/svg/card-logos/tipe-orderdetails.svg'}
+                                                    className=""
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="d-flex flex-column">
+                                                <p className="text-muted fw-bold text-hover-primary text-start fs-6 mb-0">
+                                                    Tipe Order
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div className="d-flex flex-column w-100 me-2">
+                                            <div className={``}>
+                                                <div className={`text-end fw-bold fs-5 text-muted `}>
+                                                    {subscription}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="">
+                                        <div className="d-flex align-items-center mt-2">
+                                            <div className="symbol symbol-30px me-2">
+                                                <img
+                                                    src={'/media/svg/card-logos/status-orderdetails.svg'}
+                                                    className=""
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="d-flex flex-column">
+                                                <p className="text-muted fw-bold text-hover-primary text-start fs-6 mb-0">
+                                                    Status
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div className="d-flex flex-column w-100 me-2">
+                                            <div className={``}>
+                                                <div className={`text-end fw-bold fs-5 `}>
+                                                    <Badge
+                                                        badgeColor={statusPayment === PaymentConfirmation.Confirmed ? "success" : "danger"}
+                                                        label={statusPayment}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th className="">
+                                        <div className="d-flex align-items-center mt-2">
+                                            <div className="symbol symbol-30px me-2">
+                                                <img
+                                                    src={'/media/svg/card-logos/payment-orderdetails.svg'}
+                                                    className=""
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="d-flex flex-column">
+                                                <p className="text-muted fw-bold text-hover-primary text-start fs-6 mb-0">
+                                                    Metode Pembayaran
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div className="d-flex flex-column w-100 me-2">
+                                            <div className={``}>
+                                                <div className={`text-end fw-bold fs-5 `}>
+                                                    <Buttons buttonColor="secondary" classNames="btn-sm fw-bold fs-5 me-5" >
+                                                        {buttonValue}
+                                                    </Buttons>
+                                                    {buttonPayment}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                             </tbody>
                             {/* end::Table body */}
                         </table>
