b9647c62ff5f3ea6f4133e050bb7565cbca091c5
dsaauser4 <dsaauser4@gmail.com>
2024-05-06 14:57:20 +0800

    :sparkles: Addd Class Management Page

---
+++ src/pages/admin/products/index.tsx
@@ -0,0 +1,8 @@
+import Products from "@/templates/Admin/Products";
+import { NextPage } from "next";
+
+const ProductPage: NextPage = () => {
+  return <Products />;
+};
+
+export default ProductPage;
---
+++ src/templates/Admin/Products/Products-view-model.tsx
@@ -0,0 +1,14 @@
+export const breadcrumbs = [
+  {
+    title: "Manajemen Produk",
+    path: "/admin/products",
+    isSeparator: false,
+    isActive: false,
+  },
+  {
+    title: "",
+    path: "",
+    isSeparator: true,
+    isActive: false,
+  },
+];
