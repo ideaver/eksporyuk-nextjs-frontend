d8d50b756e0fc391fd23156e7769559cee23716f
dsaauser4 <dsaauser4@gmail.com>
2024-05-21 12:09:50 +0800

    :sparkles: Create Support Chat System

--- src/_metronic/helpers/dataExamples.ts
+++ src/_metronic/helpers/dataExamples.ts
@@ -1,5 +1,8 @@
+import { User } from "@/app/service/graphql/gen/graphql";
+
+
 export interface MessageModel {
-  user: number
+  user: UserInfoModel;
   type: 'in' | 'out'
   text: string
   time: string
@@ -7,62 +10,62 @@ export interface MessageModel {
 }
 
 const defaultMessages: Array<MessageModel> = [
-  {
-    user: 4,
-    type: 'in',
-    text: 'How likely are you to recommend our company to your friends and family ?',
-    time: '2 mins',
-  },
-  {
-    user: 2,
-    type: 'out',
-    text: 'Hey there, we’re just writing to let you know that you’ve been subscribed to a repository on GitHub.',
-    time: '5 mins',
-  },
-  {
-    user: 4,
-    type: 'in',
-    text: 'Ok, Understood!',
-    time: '1 Hour',
-  },
-  {
-    user: 2,
-    type: 'out',
-    text: 'You’ll receive notifications for all issues, pull requests!',
-    time: '2 Hours',
-  },
-  {
-    user: 4,
-    type: 'in',
-    text: 'You can unwatch this repository immediately by clicking here: <a href="https://keenthemes.com">Keenthemes.com</a>',
-    time: '3 Hours',
-  },
-  {
-    user: 2,
-    type: 'out',
-    text: 'Most purchased Business courses during this sale!',
-    time: '4 Hours',
-  },
-  {
-    user: 4,
-    type: 'in',
-    text: 'Company BBQ to celebrate the last quater achievements and goals. Food and drinks provided',
-    time: '5 Hours',
-  },
-  {
-    template: true,
-    user: 2,
-    type: 'out',
-    text: '',
-    time: 'Just now',
-  },
-  {
-    template: true,
-    user: 4,
-    type: 'in',
-    text: 'Right before vacation season we have the next Big Deal for you.',
-    time: 'Just now',
-  },
+  // {
+  //   user: 4,
+  //   type: 'in',
+  //   text: 'How likely are you to recommend our company to your friends and family ?',
+  //   time: '2 mins',
+  // },
+  // {
+  //   user: 2,
+  //   type: 'out',
+  //   text: 'Hey there, we’re just writing to let you know that you’ve been subscribed to a repository on GitHub.',
+  //   time: '5 mins',
+  // },
+  // {
+  //   user: 4,
+  //   type: 'in',
+  //   text: 'Ok, Understood!',
+  //   time: '1 Hour',
+  // },
+  // {
+  //   user: 2,
+  //   type: 'out',
+  //   text: 'You’ll receive notifications for all issues, pull requests!',
+  //   time: '2 Hours',
+  // },
+  // {
+  //   user: 4,
+  //   type: 'in',
+  //   text: 'You can unwatch this repository immediately by clicking here: <a href="https://keenthemes.com">Keenthemes.com</a>',
+  //   time: '3 Hours',
+  // },
+  // {
+  //   user: 2,
+  //   type: 'out',
+  //   text: 'Most purchased Business courses during this sale!',
+  //   time: '4 Hours',
+  // },
+  // {
+  //   user: 4,
+  //   type: 'in',
+  //   text: 'Company BBQ to celebrate the last quater achievements and goals. Food and drinks provided',
+  //   time: '5 Hours',
+  // },
+  // {
+  //   template: true,
+  //   user: 2,
+  //   type: 'out',
+  //   text: '',
+  //   time: 'Just now',
+  // },
+  // {
+  //   template: true,
+  //   user: 4,
+  //   type: 'in',
+  //   text: 'Right before vacation season we have the next Big Deal for you.',
+  //   time: 'Just now',
+  // },
 ]
 
 export interface UserInfoModel {
@@ -190,12 +193,12 @@ const defaultUserInfos: Array<UserInfoModel> = [
   },
 ]
 
-const messageFromClient: MessageModel = {
-  user: 4,
-  type: 'in',
-  text: 'Thank you for your awesome support!',
-  time: 'Just now',
-}
+// const messageFromClient: MessageModel = {
+//   user: 4,
+//   type: 'in',
+//   text: 'Thank you for your awesome support!',
+//   time: 'Just now',
+// }
 
 export interface AlertModel {
   title: string
@@ -278,4 +281,4 @@ const defaultLogs: Array<LogModel> = [
   {code: '500 ERR', state: 'danger', message: 'Mail tasks', time: 'Dec 10'},
 ]
 
-export {defaultMessages, defaultUserInfos, defaultAlerts, defaultLogs, messageFromClient}
+export {defaultMessages, defaultUserInfos, defaultAlerts, defaultLogs, }
