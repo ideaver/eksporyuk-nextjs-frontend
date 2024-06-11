00b508a00ecaace887e1da49613cb4f5647b2dc1
dssauser24 <email@example.com>
2024-06-11 19:29:07 +0700

    feat: identification card download and update status

--- src/templates/Admin/Member/Detail/Profile/index.tsx
+++ src/templates/Admin/Member/Detail/Profile/index.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { KTIcon } from "@/_metronic/helpers";
-import { StudentFindOneQuery } from "@/app/service/graphql/gen/graphql";
+import {
+  IdentificationStatusEnum,
+  StudentFindOneQuery,
+} from "@/app/service/graphql/gen/graphql";
 import { formatAddress } from "@/app/service/utils/addressFormatter";
 import useForgotPassword from "@/app/service/utils/auth/forgotPasswordHook";
 import { formatDate } from "@/app/service/utils/dateFormatter";
+import EditIdentificationModal from "@/components/partials/Modals/Mutations/EditIdentificationStatusModal";
 import ForgotPasswordModal from "@/components/partials/Modals/Mutations/ForgotPasswordModal";
 import { Alert } from "@/stories/molecules/Alert/Alert";
+import { Buttons } from "@/stories/molecules/Buttons/Buttons";
+import { useState } from "react";
 
 const ProfilePage = ({
   data,
@@ -14,6 +20,26 @@ const ProfilePage = ({
 }) => {
   const userData = data?.user;
   const userAddress = userData?.addresses?.find((a) => a.isMain === true);
+
+  const handleDownloadIdentificationCard = async (
+    path: string,
+    name: string
+  ) => {
+    try {
+      const response = await fetch(path);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = `${name}-KTP.jpg`;
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const {
     forgotPasswordModalLoading,
@@ -103,9 +129,7 @@ const ProfilePage = ({
           </div>
 
           <div className="row mb-7">
-            <label className="col-lg-4 fw-bold text-muted">
-              Info Rekening
-            </label>
+            <label className="col-lg-4 fw-bold text-muted">Info Rekening</label>
 
             <div className="col-lg-8 fv-row">
               <span className="fw-bolder fs-6 text-dark">
@@ -113,6 +137,35 @@ const ProfilePage = ({
               </span>
             </div>
           </div>
+          {data?.user.identification ? (
+            <div
+              className="notice d-flex bg-light-primary justify-content-end align-content-end rounded border-primary border mb-2 p-2"
+              style={{ width: "fit-content" }}
+            >
+              <KTIcon
+                iconName="profile-user"
+                className="fs-2tx text-primary me-4 mt-2"
+              />
+              <div className="d-flex flex-stack flex-grow-1"></div>
+              <Buttons
+                classNames="me-2"
+                onClick={() =>
+                  handleDownloadIdentificationCard(
+                    `${data?.user?.identification?.personalDocumentIdPath}`,
+                    data?.user?.name as string
+                  )
+                }
+              >
+                Download KTP
+              </Buttons>
+              <Buttons
+                data-bs-toggle="modal"
+                data-bs-target="#kt_edit_status_modal"
+              >
+                Ubah Status KTP
+              </Buttons>
+            </div>
+          ) : null}
 
           <div className="notice d-flex bg-light-primary rounded border-primary border border-dashed p-6">
             <KTIcon iconName="lock" className="fs-2tx text-primary me-4" />
@@ -140,6 +193,14 @@ const ProfilePage = ({
         show={showForgotPasswordModal}
         handleSubmit={() => handleForgotPassword(userData!.email)}
         isLoading={forgotPasswordModalLoading}
+      />
+      <EditIdentificationModal
+        id={data?.user?.identification?.id as number}
+        defaultStatus={
+          data?.user.identification
+            ?.identificationStatus as IdentificationStatusEnum
+        }
+        onClick={() => {}}
       />
     </>
   );
